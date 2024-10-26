96141fa86ef7771685d8749b1696f4c1a6eddaf8
brenolapreza <brenolapreza@gmail.com>
2024-10-26 15:27:28 -0300

    feat: force-dynamic

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,8 +5,6 @@ import { marked } from "marked";
 export default async function Page({params}: IBlogPageProps) {
   const getPost = await getPageNotionByIdToMd(params.slug)
   const htmlText = marked(getPost.parent);
-  console.log(htmlText)
-
   return (
     <div>
         <div dangerouslySetInnerHTML={{ __html: htmlText }} />
